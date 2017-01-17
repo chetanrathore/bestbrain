a9f2d1d4e362db4f828717415cdb935359eb4f14
chetanrathore <chetan.rathore.dev@address.com>
2017-01-17 15:15:50 +0530

    Login vc commits

--- BestBrain/BestBrain/MVC/Controllers/LoginVC.swift
+++ BestBrain/BestBrain/MVC/Controllers/LoginVC.swift
@@ -8,16 +8,26 @@
 
 import UIKit
 
-class LoginVC: UIViewController {
+class LoginVC: UIViewController,UITextFieldDelegate {
 
     
     @IBOutlet var txtUsername: UITextField!
     @IBOutlet var txtPassword: UITextField!
-    
+    @IBOutlet weak var vwUserName: UIView!
+    @IBOutlet weak var vwPassword: UIView!
+    @IBOutlet weak var btnLogin: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.setNavigationBarHidden(true, animated: true)
+        self.txtUsername.delegate=self;
+        self.txtPassword.delegate=self;
+        
+        btnLogin.layer.shadowColor = UIColor.darkGray.cgColor
+        btnLogin.layer.shadowOffset = CGSize(width: 5.0, height: 5.0)
+        btnLogin.layer.shadowRadius = 5
+        btnLogin.layer.shadowOpacity = 1.0
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,5 +42,27 @@ class LoginVC: UIViewController {
     @IBAction func handleForgotPasswordBtn(_ sender: Any) {
     }
     
-
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        
+        if textField.tag==1
+        {
+            vwUserName.layer.masksToBounds = false
+            vwUserName.layer.shadowRadius = 3.0
+            vwUserName.layer.shadowColor = UIColor.darkGray.cgColor
+            vwUserName.layer.shadowOffset =  CGSize(width: 1.0, height: 1.0)
+            vwUserName.layer.shadowOpacity = 1.0
+        }else{
+            vwPassword.layer.masksToBounds = false
+            vwPassword.layer.shadowRadius = 3.0
+            vwPassword.layer.shadowColor = UIColor.darkGray.cgColor
+            vwPassword.layer.shadowOffset =  CGSize(width: 1.0, height: 1.0)
+            vwPassword.layer.shadowOpacity = 1.0
+        }
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        let myview = textField.superview
+        myview?.layer.shadowOpacity = 0.0;
+        return true;
+    }
 }
